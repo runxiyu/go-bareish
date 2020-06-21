3af268a3e427bd13f7be64bd67c350e44ba7ee67
Drew DeVault <sir@cmpwn.com>
2020-06-21 12:53:12 -0400

    example: decode customer from stdin

--- examples/main.go
+++ examples/main.go
@@ -2,10 +2,42 @@ package main
 //go:generate go run git.sr.ht/~sircmpwn/go-bare/cmd/gen -p main -s Time schema.bare schema.go
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	var dept = ACCOUNTING
-	log.Printf("%s: %d", dept.String(), uint8(dept))
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("read stdin: %e", err)
+	}
+	var customer Customer
+	err = customer.Decode(data)
+	if err != nil {
+		log.Fatalf("decode: %e", err)
+	}
+	var addrs []string
+	for _, addr := range customer.Address.Address {
+		if addr != "" {
+			addrs = append(addrs, addr)
+		}
+	}
+	fmt.Printf(`Customer details:
+Name: %s
+Email: %s
+Address:
+	%s
+	%s, %s
+	%s
+Orders:
+`, customer.Name, customer.Email, strings.Join(addrs, "\n"),
+	customer.Address.City, customer.Address.State,
+	customer.Address.Country)
+	for _, order := range customer.Orders {
+		fmt.Printf("- Order ID: %d\n  Quantity: %d\n",
+			order.OrderId, order.Quantity)
+	}
 }
